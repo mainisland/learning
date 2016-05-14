c6ea930eb4e68bf76dc16fb0f82da75062bc1801
liujinghaiok@sina.com <liujinghaiok@sina.com>
2016-05-14 17:18:53 +0800

    添加hessian

---
+++ sources/hessian/CscHessianClient.php
@@ -0,0 +1,176 @@
+<?php
+/*
+ * This file is part of the HessianPHP package.
+ * (c) 2004-2010 Manuel G�mez
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+include_once 'HessianFactory.php';
+include_once 'HessianTransport.php';
+
+/**
+ * Proxy to issue RPC calls to remote Hessian services 
+ */
+class CscHessianClient{
+	protected $url;
+	protected $options;
+	protected $typemap;
+	protected $factory;
+	
+	/**
+	 * Creates a new Client proxy, takes an url and an optional options object
+	 * that can also be an array
+	 * @param string $url
+	 * @param mixed $options
+	 */
+	public function __construct($url, $options = null){
+		$this->url = $url;
+		$this->options = HessianOptions::resolveOptions($options);
+		$this->typemap = new HessianTypeMap($this->options->typeMap);
+		$this->factory = new HessianFactory();
+	}
+	
+	/**
+	 * Issues a call to a remote service. It will raise a HessianFault exception if 
+	 * there is an error
+	 * @param string $method Name of the method in the remote service
+	 * @param array $arguments Optional arguments
+	 * @return mixed
+	 */
+	public function __hessianCall($method, $arguments = array()){
+	    
+		if(strpos($method, "__") === 0)
+			throw new HessianException("Cannot call methods that start with __");
+		$transport = $this->factory->getTransport($this->options);
+		$writer = $this->factory->getWriter(null, $this->options);
+		$writer->setTypeMap($this->typemap);
+		
+		$ctx = new HessianCallingContext();
+		$ctx->writer = $writer;
+		$ctx->transport = $transport;
+		$ctx->options = $this->options;
+		$ctx->typemap = $this->typemap;
+		$ctx->call = new HessianCall($method, $arguments);
+		$ctx->url = $this->url;
+		$ctx->payload = $writer->writeCall($method, $arguments);
+		$args = array($ctx);
+		foreach($this->options->interceptors as $interceptor){
+			$interceptor->beforeRequest($ctx);
+		}
+		$this->__handleCallbacks($this->options->before, $args);
+		
+		$stream = $transport->getStream($this->url, $ctx->payload, $this->options);
+		$parser = $this->factory->getParser($stream, $this->options);
+		$parser->setTypeMap($this->typemap);
+		// TODO deal with headers, packets and the rest of aditional stuff
+		$ctx->parser = $parser;
+		$ctx->stream = $stream;
+		
+		try{
+			$result = $parser->parseTop();
+		} catch(Exception $e){
+			$ctx->error = $e;
+		}
+		foreach($this->options->interceptors as $interceptor){
+			$interceptor->afterRequest($ctx);
+		}
+		$this->__handleCallbacks($this->options->after, $args);
+		
+		if($ctx->error instanceof Exception) {
+		    //Yii::log( $ctx->error, CLogger::LEVEL_ERROR, $this->url.'::'.$method );
+			throw $ctx->error;
+		}
+		
+		/// end_log //////
+// 		if ( HESSIAN_LOG === 'On' ) {
+// 		    list($unusec,$sec) = explode(' ', microtime());
+// 		    $endTime = (float)$unusec - (float)$usec;
+// 		    $row = sprintf("%s %ss\n", $uuid, $endTime);
+// 		    $fp = fopen($filepath, 'a');
+// 		    flock($fp,LOCK_EX);
+// 		    fwrite($fp, $row);
+// 		    flock($fp,LOCK_UN);
+// 		    fclose($fp);
+// 		}
+		/// end_log //////
+		
+		
+		/// begin_log //////
+// 		defined('HESSIAN_LOG')||define('HESSIAN_LOG', Yii::app()->request->getQuery('hessian_log','Off'));
+// 		if ( HESSIAN_LOG === 'On' ||  Yii::app()->request->getQuery('hessian_log') === 'On') {
+// 			$uuid = CSCHelper::uuid();
+// 			$filepath = CSC_PRO_LOG_DIR.'/hessian-'.date('Y m d H-i-s').'.log';
+// 			list($usec,$sec) = explode(' ', microtime());
+// 			$beginTime = date( 'Y-m-d H:i:s', (int)$sec );
+// 			/*
+// 			 * 增加日志文件参数
+// 			* date:2011-11-21
+// 			* anthor:xugx
+// 			*/
+// 			//     		$row = sprintf("\n%s\n%s %s::%s\n", $beginTime, $uuid, $this->url, $method);
+// 			ob_start();
+// 			$row =  array(
+// 					'method' => $method,
+// 					'interface' => $this->url,
+// 					'php param' => $arguments,
+// 					'java return' => $result
+// 			);
+// 			echo "///////////////////////同一秒访问的多个接口日志会写在同一个文件里////////////////////////////////////////\n\r";
+// 			echo "date:".date('Y年m月d日 H时i分s秒')."\n\r";
+// 			print_r($row);
+// 			$row = ob_get_clean();
+// 			$fp = fopen($filepath, 'a');
+// 			flock($fp,LOCK_EX);
+// 			fwrite($fp, $row);
+// 			flock($fp,LOCK_UN);
+// 			fclose($fp);
+// 		}
+		/// begin_log //////
+		
+		return $result;
+	}
+	
+	private function __handleCallbacks($callbacks, $arguments){
+		if(!$callbacks)
+			return;
+		if(is_callable($callbacks)){
+			return call_user_func_array($callbacks, $arguments);
+		}
+		if(!is_array($callbacks))
+			return;
+		foreach($callbacks as $call){
+			if(is_callable($call)){
+				return call_user_func_array($call, $arguments);
+			}
+		}
+	}
+	
+	/**
+	 * Magic function wrapper for the remote call. It will fail if called
+	 * with methods that start with __ which are conventionally private
+	 * @param string $method
+	 * @param array $arguments
+	 * @return mixed Result of the remote call
+	 */
+	public function __call($method, $arguments){
+		return $this->__hessianCall($method, $arguments); 
+	}
+	
+	/**
+	 * Returns this client's current options
+	 * @return HessianOptions
+	 */
+	public function __getOptions(){
+		return $this->options;
+	}
+	
+	/**
+	 * Returns the current typemap for this client
+	 * @return HessianTypeMap
+	 */
+	public function __getTypeMap(){
+		return $this->typemap;
+	}
+}
---
+++ 服务器相关/hessian.md
@@ -0,0 +1,128 @@
+#Hessian
+
+轻量级的Remoting Onhttpg工具，采用二进制RPC协议，多语言支持；
+
+
+##php调用java接口实例代码
+<?php
+include_once CSC_LIBS_DIR . '/hessian/HessianClient.php';
+
+class BaseHessianClient extends HessianClient {
+
+	private static $_self;
+	protected $aliases;
+	protected $HessianOptions;
+	protected $infoException;
+
+	/**
+	 * 对父类构造函数进行改造，忽略传入的参数直接从设置中进行初始化
+	 * @param string $url
+	 * @param string $options
+	 */
+	public function __construct($url='', $options = null){
+		$this->options = $this->transformOptions();
+		$this->typemap = new HessianTypeMap($this->options->typeMap);
+		$this->factory = new HessianFactory();
+	}
+	
+	/**
+	 * 单例初始化工作
+	 */
+	public static final function initialize() {
+		$className = get_called_class();
+		return new $className;
+	}
+
+	/**
+	 * 进行事件触发及别名的转换工作，之后调用远程的操作方法
+	 * @see HessianClient::__call()
+	 */
+	public function __call($method,$arguments) {
+		$this->infoException = null;
+		if(!$this->runBeginEvent($method, $arguments)) {
+			return false;
+		}
+		
+		try {
+			$method = $this->transformMethod($method);
+			return $this->__hessianCall($method, $arguments);
+		}
+		catch (HessianFault $Ex) {
+			if ( $Ex->getMessage() == 'CscBizException' ) { // 如果消息为CscBizException，我们php端无需处理
+				$this->infoException = $Ex->detail;
+				return 'CscBizException';
+			}
+
+// 			throw new CHttpException(500, Yii::t('yii', 'Has CscAppException by {class}::{action}', array('{class}'=>get_called_class(),'{action}'=>$method)));
+			// 这里需要兼容，java抛异常，把错误的异常信息放在message中 .  By Bear
+			throw new CHttpException(500, $Ex->getMessage()); 
+		}
+	}
+	
+	/**
+	 * 更新HessianOptions设置
+	 * @param array $options
+	 * @throws CHttpException
+	 */
+	public function setOptions($options) {
+		if (!is_array($options)) {
+			throw new CHttpException(500, Yii::t('yii','Invalid arguments $options in {class}::{func}',array('{class}'=>__CLASS__,'{func}'=>__FUNCTION__)));
+		}
+		if ( $options ) {
+			$options = array_merge($this->HessianOptions, $options);
+			$this->options = $this->transformOptions($options);
+			$this->typemap = new HessianTypeMap($this->options->typeMap);
+		}
+	}
+	
+	/**
+	 * 进行HessianOptions对象初始化工作
+	 * @return HessianOptions|NULL
+	 */
+	private function transformOptions($HessianOptions=null){
+		$options = new HessianOptions();
+		if( is_null($HessianOptions) ) {
+		    $HessianOptions = $this->HessianOptions;
+		}
+		
+		if(!is_array($HessianOptions)) {
+		    $HessianOptions = array();
+		}
+		array_push($HessianOptions, 'com.csc.appframework.exception.dto.CscAppExceptionDTO');
+		foreach( $HessianOptions as $item ) {
+		    $key = substr($item, strrpos($item,'.')+1);
+		    $options->typeMap[$key] = $item;
+		}
+		return HessianOptions::resolveOptions($options);
+	}
+	
+	/**
+	 * 执行方法事件触发
+	 * @param string $method
+	 * @param array $paramsArg
+	 * @return mixed
+	 */
+	private function runBeginEvent($method, & $paramsArg) {
+		$event = 'OnBegin'.ucfirst($method);
+		if ( method_exists($this, $event) ) {
+			return call_user_func_array(array($this,$event), array(& $paramsArg));
+		}
+		return true;
+	}
+	
+	/**
+	 * 方法名进行别名转换
+	 * @param string $methodName
+	 * @return string
+	 */
+	private function transformMethod($methodName) {
+		if ( !is_null($this->aliases) && is_array($this->aliases) && array_key_exists($methodName, $this->aliases) ) {
+			return $this->aliases[$methodName];
+		}
+		return $methodName;
+	}
+}
+
+
+
+[参考文档](http://hessian.caucho.com/)
